66e507ccfa4c310497a1bae65e1f1a8cd3c72758
Luka Urbanc <lu02924@student.uni-lj.si>
2024-10-06 22:26:46 +0200

    Added Fibonacci
    Fun times were had

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!";
+    unsigned int n, mod;
+    unsigned long long m[4]={1,0,0,1}, a[4];
+    std::cout << "Hello world! Enter modulo: ";
+    std::cin >> mod;
+    std::cout << "Enter index of Fibonacci number: ";
+    std::cin >> n;
+    
+    for (int i = 0; i < 32; i++) {
+        a[0]=m[0]*m[0]+m[1]*m[2]%mod;
+        a[1]=m[0]*m[1]+m[1]*m[3]%mod;
+        a[2]=m[2]*m[0]+m[3]*m[2]%mod;
+        a[3]=m[2]*m[1]+m[3]*m[3]%mod;
+        if (n&(1<<(31-i))) {
+            m[0]=a[0]+a[1]%mod;
+            m[1]=a[0]%mod;
+            m[2]=a[2]+a[3]%mod;
+            m[3]=a[2]%mod;
+        } else {
+            m[0]=a[0]%mod;
+            m[1]=a[1]%mod;
+            m[2]=a[2]%mod;
+            m[3]=a[3]%mod;
+        }
+    }
+    std::cout << "F_" << n << " = " << m[1] << " (mod " << mod << ")" << std::endl;
     return 0;
 }
